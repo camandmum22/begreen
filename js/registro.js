37f7b4189123bc66938b197ac25a1d1a9a02bde3
csacanam <csacanam@gmail.com>
2015-02-13 00:53:53 -0500

    Se permite registar la compra de un cliente ya registrado previamente

--- js/registro.js
+++ js/registro.js
@@ -1,5 +1,5 @@
-// 1. Registro de clientes
-function register(){
+// 1. Registro de clientes nuevos
+function registrarClienteNuevo(){
 	Parse.initialize("LUSRBvC9SjQBrv2YEPCoCMO3gDbDVzy415qrZumJ", "GQyuwQsf0IBgbeTCzWKDqr5TxOwral7IvdlYbT6J");    
 	var Persona = Parse.Object.extend("Persona");
 	var persona = new Persona();
@@ -13,98 +13,99 @@ function register(){
 		
 		
 		//Verificar si el usuario ya existe
-	    var query = new Parse.Query(Persona);
-	    query.equalTo("cedula", document.formRegister.cedula.value);
-	    query.first({
-	      success: function(object) {
+		var query = new Parse.Query(Persona);
+		query.equalTo("cedula", document.formRegister.cedula.value);
+		query.first({
+			success: function(object) {
 			  
-			//El usuario ya existe. No se puede crear.
-	        if (object) 
-			{
-				alert('Ya existe un usuario registrado con esa cédula');
+				//El usuario ya existe. No se puede crear.
+				if (object) 
+				{
+					alert('Ya existe un usuario registrado con esa cédula');
 	          
-			//El usuario no existe. Se puede crear.
-	        } else 
-			{
+					//El usuario no existe. Se puede crear.
+				} else 
+				{
 
-				//Guardar usuario
-				persona.save(null, {
-					success:function(persona){				
+					//Guardar usuario
+					persona.save(null, {
+						success:function(persona){				
 			
-						//Agregar kit de sembrado
-						if(document.formRegister.kitsembrado.value > 0)
-						{
-							var Venta = Parse.Object.extend("Venta");
-							var venta = new Venta();
-							venta.set("cedVendedor",Parse.User.current().get('username'));
-							venta.set("cedCliente",persona.get('cedula'));
-				
-							//Agregar recomendado
-							if(document.formRegister.recomendado.value!=null && document.formRegister.recomendado.value!="")
+							//Agregar kit de sembrado
+							if(document.formRegister.kitsembrado.value > 0)
 							{
-								venta.set("cedRecomendado",document.formRegister.recomendado.value);
+								var Venta = Parse.Object.extend("Venta");
+								var venta = new Venta();
+								venta.set("cedVendedor",Parse.User.current().get('username'));
+								venta.set("cedCliente",persona.get('cedula'));
+				
+								//Agregar recomendado
+								if(document.formRegister.recomendado.value!=null && document.formRegister.recomendado.value!="")
+								{
+									venta.set("cedRecomendado",document.formRegister.recomendado.value);
+								}
+				
+								venta.set("codProd","PROD1");
+								venta.set("cantidad", parseInt(formRegister.kitsembrado.value));
+								venta.save();
 							}
-				
-							venta.set("codProd","PROD1");
-							venta.set("cantidad", parseInt(formRegister.kitsembrado.value));
-							venta.save();
-						}
 			
-						//Agregar bambues
-						if(document.formRegister.bambues.value > 0)
-						{
-							var Venta = Parse.Object.extend("Venta");
-							var venta = new Venta();
-							venta.set("cedVendedor",Parse.User.current().get('username'));
-							venta.set("cedCliente",persona.get('cedula'));
-				
-							//Agregar recomendado
-							if(document.formRegister.recomendado.value!=null && document.formRegister.recomendado.value!="")
+							//Agregar bambues
+							if(document.formRegister.bambues.value > 0)
 							{
-								venta.set("cedRecomendado",document.formRegister.recomendado.value);
+								var Venta = Parse.Object.extend("Venta");
+								var venta = new Venta();
+								venta.set("cedVendedor",Parse.User.current().get('username'));
+								venta.set("cedCliente",persona.get('cedula'));
+				
+								//Agregar recomendado
+								if(document.formRegister.recomendado.value!=null && document.formRegister.recomendado.value!="")
+								{
+									venta.set("cedRecomendado",document.formRegister.recomendado.value);
+								}
+				
+								venta.set("codProd","PROD2");
+								venta.set("cantidad", parseInt(formRegister.bambues.value));
+								venta.save();
 							}
-				
-							venta.set("codProd","PROD2");
-							venta.set("cantidad", parseInt(formRegister.bambues.value));
-							venta.save();
-						}
 			
-						//Agregar cactus
-						if(document.formRegister.cactus.value > 0)
-						{
-							var Venta = Parse.Object.extend("Venta");
-							var venta = new Venta();
-							venta.set("cedVendedor",Parse.User.current().get('username'));
-							venta.set("cedCliente",persona.get('cedula'));
-				
-							//Agregar recomendado
-							if(document.formRegister.recomendado.value!=null && document.formRegister.recomendado.value!="")
+							//Agregar cactus
+							if(document.formRegister.cactus.value > 0)
 							{
-								venta.set("cedRecomendado",document.formRegister.recomendado.value);
-							}
+								var Venta = Parse.Object.extend("Venta");
+								var venta = new Venta();
+								venta.set("cedVendedor",Parse.User.current().get('username'));
+								venta.set("cedCliente",persona.get('cedula'));
 				
-							venta.set("codProd","PROD3");
-							venta.set("cantidad", parseInt(formRegister.cactus.value));
-							venta.save();
-						}
+								//Agregar recomendado
+								if(document.formRegister.recomendado.value!=null && document.formRegister.recomendado.value!="")
+								{
+									venta.set("cedRecomendado",document.formRegister.recomendado.value);
+								}
+				
+								venta.set("codProd","PROD3");
+								venta.set("cantidad", parseInt(formRegister.cactus.value));
+								venta.save();
+							}
 
 
-						
-						document.getElementById('formRegister').reset();
+							//Limpiar campos
+							document.getElementById('formRegister').reset();
 
-						alert('Usuario creado: ' + persona.get('cedula'));
+							//Mostrar mensaje al usuario
+							alert('Usuario creado: ' + persona.get('cedula'));
 			
-					},
-					error:function(persona, error){
-						alert('El usuario no puede ser creado');
-					}
-				});
-	        }
-	      },
-	      error: function(error) {
-	        response.error("Error registrando el usuario. Intente más tarde.");
-	      }
-	    });
+						},
+						error:function(persona, error){
+							alert('El usuario no puede ser creado');
+						}
+					});
+				}
+			},
+			error: function(error) {
+				response.error("Error registrando el usuario. Intente más tarde.");
+			}
+		});
 	
 
 	}
@@ -121,7 +122,7 @@ function validarVendedor(){
 	var currentUser = Parse.User.current();
 	if (currentUser) {
 		$(".success").show();
-		document.getElementById("userLogged").innerHTML = 'Sesión iniciada como: ' + currentUser.get('name');		 
+		document.getElementById("userLogged").innerHTML = 'Vendedor: <strong>' + currentUser.get('name') + '</strong>';		 
 	} 
 	else{
 		var newUrl = "login.html";
@@ -138,3 +139,88 @@ function logout(){
 	document.location.href = newUrl;
 	Parse.User.logOut();
 }
+
+//4. Registrar compra
+function registrarCompra(){
+	
+	if(document.formCompra.cedula.value!=null && document.formCompra.cedula.value != "")
+	{
+		Parse.initialize("LUSRBvC9SjQBrv2YEPCoCMO3gDbDVzy415qrZumJ", "GQyuwQsf0IBgbeTCzWKDqr5TxOwral7IvdlYbT6J");    
+		var Persona = Parse.Object.extend("Persona");
+		var query = new Parse.Query(Persona);
+		query.equalTo("cedula", document.formCompra.cedula.value);
+		query.first({
+			success: function(object) {
+		  
+				//El usuario existe. Se puede registrar la compra.
+				if (object) 
+				{
+			
+		
+					//Agregar kit de sembrado
+					if(document.formCompra.kitsembrado.value > 0)
+					{
+						var Venta = Parse.Object.extend("Venta");
+						var venta = new Venta();
+						venta.set("cedVendedor",Parse.User.current().get('username'));
+						venta.set("cedCliente",document.formCompra.cedula.value);
+						venta.set("codProd","PROD1");
+						venta.set("cantidad", parseInt(formCompra.kitsembrado.value));
+						venta.save();
+					}
+		
+					//Agregar bambues
+					if(document.formCompra.bambues.value > 0)
+					{
+						var Venta = Parse.Object.extend("Venta");
+						var venta = new Venta();
+						venta.set("cedVendedor",Parse.User.current().get('username'));
+						venta.set("cedCliente",document.formCompra.cedula.value);
+						venta.set("codProd","PROD2");
+						venta.set("cantidad", parseInt(formCompra.bambues.value));
+						venta.save();
+					}
+		
+					//Agregar cactus
+					if(document.formCompra.cactus.value > 0)
+					{
+						var Venta = Parse.Object.extend("Venta");
+						var venta = new Venta();
+						venta.set("cedVendedor",Parse.User.current().get('username'));
+						venta.set("cedCliente",document.formCompra.cedula.value);
+						venta.set("codProd","PROD3");
+						venta.set("cantidad", parseInt(formCompra.cactus.value));
+						venta.save();
+					}
+
+
+					//Limpiar campos
+					document.getElementById('formCompra').reset();
+
+					//Mostrar mensaje al usuario
+					alert('Venta realizada');
+		
+						
+          
+				//El usuario no existe. Se debe registrar.
+				} else 
+				{
+					alert('El usuario no existe. Debe registrarse primero');
+					document.getElementById('formCompra').reset();
+					
+
+				}
+			},
+			error: function(object, error) {
+				response.error("Error obteniendo información. Intente más tarde.");
+			}
+		});
+	
+	}
+	else
+	{
+		alert('No puede dejar campos vacíos');
+	
+	}
+}
+
